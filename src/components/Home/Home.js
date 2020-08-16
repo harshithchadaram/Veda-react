0facb9dfbf461b6053e8e3f31d4774cee6ab5f6c
harshithchadaram <58565616+harshithchadaram@users.noreply.github.com>
2020-08-16 17:28:52 +0530

    home page implementation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import Typography from "@material-ui/core/Typography";
 
 function Home() {
   return (
-    <div className='d-flex flex-column w-100 h-100'>
+    <div className='d-flex flex-column w-100 '>
       <div>
         <div className="d-flex flex-column justify-content-center align-items-center bg-img-home ">
           {/* <p>sdcsdcdscdfbcdbfghcvdfg</p> */}
@@ -24,15 +24,25 @@ function Home() {
             </IconButton>
           </form>
         </div>
-        <div className="d-flex flex-row-reverse bg-img-1 h-100">
-          <Typography variant="h4" className='text-light mb-3 search-heading' noWrap>
-            Pick your favorite food
+        <div className="d-flex flex-row-reverse bg-img-1 align-items-center mx-5 mt-4">
+          <div className='d-flex flex-column w-50 p-5'>
+            <Typography variant="h5" className='font-weight-bold mb-3' noWrap>
+              Find Your Favorite Food
     </Typography>
+            <Typography variant="subtitle1" className='' >
+              From your neighborhood sushi spot to the burger and fries you crave, choose from over 300,000 local and national favorites across the U.S. and Canada
+    </Typography>
+          </div>
         </div>
-        <div className="d-flex bg-img-2 h-100">
-          <Typography variant="h4" className='text-light mb-3 search-heading' noWrap>
-            Pick your favorite food
+        <div className="d-flex bg-img-2 align-items-center mx-5 mb-4">
+          <div className='d-flex flex-column w-50 p-5'>
+            <Typography variant="h5" className='font-weight-bold mb-3' noWrap>
+              Become A Partner With Us
     </Typography>
+            <Typography variant="subtitle1" className='' >
+              From your neighborhood sushi spot to the burger and fries you crave, choose from over 300,000 local and national favorites across the U.S. and Canada
+    </Typography>
+          </div>
         </div>
       </div>
     </div>
